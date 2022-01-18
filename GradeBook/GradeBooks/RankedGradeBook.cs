270eda7da9f447eeceda101b703bfaca2ba3735e
NickSondrup <nicholas.sondrup@gmail.com>
2022-01-18 11:55:14 -0700

    new methods

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿
 using GradeBook.Enums;
+using System;
 
 namespace GradeBook.GradeBooks
 {
@@ -8,6 +9,16 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+            }
+
+            return base.GetLetterGrade(averageGrade);
         }
     }
 }
